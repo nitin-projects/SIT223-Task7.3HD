09e648c202872f660405911fa12bf7de73204452
nitin-projects <nitin4848.se24@chitkara.edu.in>
2026-04-16 02:27:54 +0530

    Create App.jsx

---
+++ Frontend/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import VideoConsent from './VideoConsent'; 
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', fontFamily: 'sans-serif' }}>Rural Telemedicine Platform</h1>
+      
+      {/* Yahan hum apne naye component ko screen par bula rahe hain */}
+      <VideoConsent />
+      
+    </div>
+  );
+}
+
+export default App;
